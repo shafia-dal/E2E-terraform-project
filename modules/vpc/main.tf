13621e8177f33dca7b5ad6aaec07538ae1b564fa
safiya <shafiadal185@gmail.com>
2025-03-26 10:15:15 +0000

    hange sg name in vpc main

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -122,13 +122,13 @@ resource "aws_vpc_security_group_ingress_rule" "allow_alb_traffic" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.e2e-server-sg.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.e2e-server-sg.id
   cidr_ipv6         = "::/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
