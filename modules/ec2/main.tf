8072a623385661cf1d52e431c86b38bdf9b92d24
jayesh <jayeshnaphade00@gmail.com>
2025-03-28 19:18:54 +0530

    userscript

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -8,30 +8,13 @@ resource "aws_instance" "e2e-project" {
   tags = {
     Name = var.instance_name
   }
+user_data                   = data.template_file.instance_provision.rendered
 
-  
-
-     user_data = <<-EOF
-  #!/bin/bash
-  set -euo pipefail
-  echo "running scripts"
-  sudo apt update -y
-  sudo apt install -y nfs-common
-  sudo mkdir /mnt/efs
-  
-  sudo mount -t nfs4 -o nfsvers=4.1 ${var.efs_id}:/ /mnt/efs
-  mount | grep efs
-
-
-  if dpkg -s mysql-server > /dev/null 2>&1; then
-  echo "MySQL server is installed."
-else
-  echo "MySQL server is NOT installed."
-fi
-    sudo apt-get update -y
-    sudo apt-get install mysql-client -y
-    mysql -h "${var.rds_endpoint}" -u "${var.rds_username}" -p "${var.rds_password}" -e "SELECT 1;"
-
-  EOF
+}
+data "template_file" "instance_provision" {
+  template = file("${path.module}/userscipt.sh")
 }
 
+# data "template_file" "instance_provision" {
+#   template = file("${path.module}/../scripts/instance_provisioning.bash")
+# }
