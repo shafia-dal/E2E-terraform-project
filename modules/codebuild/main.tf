e8d44cbefa026027a2434e0a595a49b6880a2a48
jayesh <jayeshnaphade00@gmail.com>
2025-04-08 17:44:50 +0530

    docker login and pass changes

--- modules/codebuild/main.tf
+++ modules/codebuild/main.tf
@@ -1,7 +1,33 @@
 ######## S3 ##############
 resource "aws_s3_bucket" "e2e_artifect" {
   bucket = var.artifect_bucket
+  # policy = file("bucket-policy.json")
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowCodeBuildToAccessS3",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::091846656105:role/e2e_codebuild_role"
+            },
+            "Action": [
+                "s3:GetObject",
+                "s3:PutObject",
+                "s3:ListBucket",
+                "s3:GetObjectVersion"
+            ],
+            "Resource": [
+                "arn:aws:s3:::e2e-artifect-bucket",
+                "arn:aws:s3:::e2e-artifect-bucket/*"
+            ]
+        }
+    ]
 }
+EOF
+}
+
 ############# codebuild iam role ##########
 resource "aws_iam_role" "e2e_codebuild_role" {
   name = "e2e_codebuild_role"
@@ -32,13 +58,40 @@ resource "aws_iam_role_policy" "e2e_codebuild_iam_policy" {
     {
       "Effect": "Allow",
       "Resource": [
-        "*",
-        "arn:aws:ecr:us-east-1:091846656105:repository/my-nodejs-repo"
+        "arn:aws:logs:us-east-1:091846656105:log-group:/aws/codebuild/e2e_codebuild_project",
+        "arn:aws:logs:us-east-1:091846656105:log-group:/aws/codebuild/e2e_codebuild_project:*"
+        ],
+      "Action": [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Resource": [
+          "arn:aws:s3:::codepipeline-us-east-1-*"
       ],
       "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:GetObjectVersion",
+          "s3:GetBucketAcl",
+          "s3:GetBucketLocation"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+          "codebuild:StartBuild",
+          "codebuild:CreateReportGroup",
+          "codebuild:CreateReport",
+          "codebuild:UpdateReport",
+          "codebuild:BatchPutTestCases",
+          "codebuild:BatchPutCodeCoverages"
+      ],
+      "Resource": [
+          "arn:aws:codebuild:us-east-1:091846656105:report-group/e2e_codebuild_project-*"
       ]
     },
     {
@@ -57,27 +110,24 @@ resource "aws_iam_role_policy" "e2e_codebuild_iam_policy" {
     {
       "Effect": "Allow",
       "Action": [
-        "ec2:CreateNetworkInterfacePermission"
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetRepositoryPolicy",
+          "ecr:DescribeRepositories",
+          "ecr:ListImages",
+          "ecr:DescribeImages",
+          "ecr:BatchGetImage",
+          "ecr:GetLifecyclePolicy",
+          "ecr:GetLifecyclePolicyPreview",
+          "ecr:ListTagsForResource",
+          "ecr:DescribeImageScanFindings",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:PutImage"
       ],
-      "Resource": [
-        "arn:aws:ec2:us-east-1:123456789012:network-interface/*"
-      ],
-      "Condition": {
-        "StringEquals": {
-          "ec2:${var.private_subnet}":"",
-          "ec2:AuthorizedService": "codebuild.amazonaws.com"
-        }
-      }
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:*"
-      ],
-      "Resource": [
-        "${aws_s3_bucket.e2e_artifect.arn}",
-        "${aws_s3_bucket.e2e_artifect.arn}/*"
-      ]
+      "Resource": "*"
     }
   ]
 }
@@ -92,7 +142,7 @@ resource "aws_codebuild_project" "e2e_codebuild_project" {
   service_role  = "${aws_iam_role.e2e_codebuild_role.arn}"
 
   artifacts {
-    type = "CODEBUILD"
+    type = "NO_ARTIFACTS"
   }
 
   cache {
@@ -120,13 +170,13 @@ resource "aws_codebuild_project" "e2e_codebuild_project" {
     buildspec = "buildspec.yml"
   }
 
-  vpc_config {
-    vpc_id = var.vpc_id
+  # vpc_config {
+  #   vpc_id = var.vpc_id
 
-    subnets = [var.private_subnet] 
-    security_group_ids = [aws_security_group.cicd_sg.id]
+  #   subnets = [var.private_subnet] 
+  #   security_group_ids = [aws_security_group.cicd_sg.id]
    
-  }
+  # }
 }
 # resource "aws_codebuild_project" "e2e-codebuild-project" {
 #   name         = "e2e-codebuild"
@@ -152,28 +202,28 @@ resource "aws_codebuild_project" "e2e_codebuild_project" {
 # }
 
 ####### security Group ################
-resource "aws_security_group" "cicd_sg" {
-  name        = "cicd_security_group"
-  description = "Security group for CodeBuild in CICD"
-  vpc_id      = var.vpc_id
+# resource "aws_security_group" "cicd_sg" {
+#   name        = "cicd_security_group"
+#   description = "Security group for CodeBuild in CICD"
+#   vpc_id      = var.vpc_id
 
-  # No inbound rules needed
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = 0
-    cidr_blocks = []
-  }
+#   # No inbound rules needed
+#   ingress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = 0
+#     cidr_blocks = []
+#   }
 
-  # Allow all outbound traffic (needed for S3, ECR, RDS, etc.)
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   # Allow all outbound traffic (needed for S3, ECR, RDS, etc.)
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name = "CICD Security Group"
-  }
-}
+#   tags = {
+#     Name = "CICD Security Group"
+#   }
+# }
